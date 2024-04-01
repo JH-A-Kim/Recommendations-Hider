986b42f6510368d8dcfe77c36ba7b86448735d8a
jak28 <jak28@sfu.ca>
2024-04-01 13:01:34 +0800

    comment about visibility made

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
 const background = document.getElementById('secondary');
 
 background.style.display = 'none';
+//visibility = hidden will hide it and not change the amount of space the website takes
